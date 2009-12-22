8cee58770f58bb41548294f749c788e8c57109df
Rayne <Rayne@acidrayne.net>
2009-12-22 09:26:01 -0600

    Lots of work done on setMarks.

--- src/Player.hs
+++ src/Player.hs
@@ -2,6 +2,7 @@ module Player where
 
 import qualified Data.Map as M
 import Data.List (intercalate)
+import Control.Monad
 
 type Score = Int
 
@@ -39,11 +40,14 @@ instance Show Card where
 getMap :: Card -> M.Map Int Mark
 getMap (Card m _) = m
 
+setMap :: Card -> M.Map Int Mark -> Card
+setMap (Card _ s) m = Card m s 
+
 getScore :: Card -> Score
 getScore (Card _ s) = s
 
 setScore :: Card -> Score -> Card
-setScore (Card m _) s = Card m s
+setScore (Card m _) = Card m
 
 data Player = Player String Card
               deriving (Show)
@@ -54,18 +58,31 @@ name (Player s _) = s
 card :: Player -> Card
 card (Player _ c) = c
 
+setCard :: Player -> Card -> Player
+setCard (Player s _) = Player s
+
 data ScoreCard = ScoreCard Player Player
 
 instance Show ScoreCard where
-  show (ScoreCard (Player _ c) (Player _ c2)) =
-    intercalate "\n" $
-    zipWith (++) (map (\x -> x ++ replicate (12 - length x) ' ') $ lines $ show c)
-            (lines $ show c2)
+  show (ScoreCard (Player s c) (Player s2 c2)) =
+    intercalate "\n" $  heading : zipWith (++) 
+    (map (\x -> x ++ replicate (20 - length x) ' ') $ lines $ show c)
+    (lines $ show c2)
+
+    where heading = s ++ replicate (20 - length s) ' ' ++ s2
     
   
 ------------------------------------------------------
 -- Code to work with the types. ----------------------
 ------------------------------------------------------
 
-setMarks :: Player -> Player -> [(Int, Mark)] -> Card
-setMarks p p2 darts = undefined -- TODO
+setMarks :: Player -> Player -> [(Int, Mark)] -> Player
+setMarks p p2 darts = setCard p $ setMap pc1 $ foldr step pc1 darts
+    where (pc1,pc2) = (card p, getMap (card p2))
+          step (n,m) ys = let (Just p2cur) = M.lookup n pc2
+                              (Just p1cur) = M.lookup n $ getMap ys
+                          in if ((&&) `on` (== Closed)) p2cur p1cur
+                             then ys
+                             else if p1curr < Closed
+                                  then setMap ys $ M.update (const (Just (p1curr + m))) n getMap ys
+                                  else setScore ys $ getScore ys + 3
