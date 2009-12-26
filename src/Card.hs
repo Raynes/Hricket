b83e1d4311ff370d73f10bbcfbe80e53bf7b5d9d
Rayne <Rayne@acidrayne.net>
2009-12-26 13:50:28 -0600

    Added Card.hs. Moved relevant code to Card.hs

---
+++ src/Card.hs
@@ -0,0 +1,27 @@
+module Card where
+
+import Mark
+import qualified Data.Map as M
+
+type Score = Int
+
+data Card = Card (M.Map Int Mark) Score
+
+instance Show Card where
+  show (Card m s) = foldl step "" (M.toList m) ++ "Score: " ++ show s
+    where step xs (n,mark) = xs ++ (show n ++ ": ") ++ show mark ++ "\n"
+
+createCard :: Card
+createCard = Card (M.fromList (zip [15..21] (replicate 7 None))) 0
+
+getMap :: Card -> M.Map Int Mark
+getMap (Card m _) = m
+
+setMap :: Card -> M.Map Int Mark -> Card
+setMap (Card _ s) m = Card m s 
+
+getScore :: Card -> Score
+getScore (Card _ s) = s
+
+setScore :: Card -> Score -> Card
+setScore (Card m _) = Card m
