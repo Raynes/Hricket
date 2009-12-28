fc4cfeaa4e8248bd3ad4ea0f3d5d39b049c8d9ee
Rayne <Rayne@acidrayne.net>
2009-12-28 08:02:53 -0600

    First roll of getValidInput. Works, but needs to make sure that all the string contains is numbers.

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ import GameCard
 import Player
 import Game
 import System.IO
+import Data.Char (digitToInt, isSpace)
 import Data.IORef (newIORef, readIORef, writeIORef, IORef)
 
 data GState = GState {gamecard :: GameCard, cplayer :: Int
@@ -36,12 +37,14 @@ main = do
 
 prompt :: IO (IORef GState)
 prompt = do
-  putStr "Welcome to Hricket. The Haskell cricket scoring application.\n\n\
-            \Please enter player 1's name: "
+  putStrLn "Welcome to Hricket. The Haskell cricket scoring application.\n\n\
+         \Please enter player 1's name: "
   p1 <- getLine
-  putStr "\nPlease enter player 2's name: "
+  putStrLn "\nPlease enter player 2's name: "
   p2 <- getLine
+  putStr "\n"
   newIORef $ newGame p1 p2
+
 
 dartPrompt gst = do
   gsraw' <- readIORef gst
@@ -50,9 +53,23 @@ dartPrompt gst = do
   let pn = cplayer gsraw
   print gstate
   putStrLn "\nEnter the dart you hit, a single space, and the number of markings.\
-            \\nFor example: 15 Three or 15 One or 15 Two or 0 None for nothing.\n"
+            \\nFor example: 15 3 ,15 2, 15 1 or 0 for nothing.\n"
   ds <- getLine
   let marked = mark (getPlayer pn gstate) (getPlayer 2 gstate) ds
   writeIORef gst gsraw {gamecard = (setPlayer pn gstate marked)
                        ,cplayer = (if ctime gsraw == 0 then increment pn else pn)}
   return gst
+
+getValidInput :: Int -> IO String
+getValidInput n = helper n ""
+    where helper 1 s = return s
+          helper 0 s = do
+            x <- getLine
+            if any isSpace x
+             then let (sub, end) = break isSpace x
+                  in if read sub > 21 || read end > 3
+                     then putStrLn "Invalid input. Please try again." >> helper 0 s
+                     else helper 1 x
+             else if read x == 0 
+                  then helper 1 x
+                  else putStrLn "Invalid input. Please try again." >> helper 0 s
